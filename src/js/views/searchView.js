24b5645f9adf457d2580cd91df4743cf48a74cee
Solomon Melaku <selomon44@gmail.com>
2021-10-26 18:02:05 +0300

    Netlify Tweaks1

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,7 +9,7 @@ class SearchView extends View {
     return query;
   }
 
-  #clearInput() {
+  clearInput() {
     this._perentElement.querySelector('.search__field').value = '';
   }
   addHandlerSearch(handler) {
